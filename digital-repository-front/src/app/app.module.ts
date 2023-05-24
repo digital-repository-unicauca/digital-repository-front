964eadcecd5ca19df966073820cb26e2f6b486f3
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2023-05-24 15:14:42 -0500

    menu navegation

--- digital-repository-front/src/app/app.module.ts
+++ digital-repository-front/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componets/home/home.component';
+import { SearcContractsComponent } from './componets/searc-contracts/searc-contracts.component';
+import { SidebarComponent } from './componets/sidebar/sidebar.component';
+
+//import icons
+import { LucideAngularModule,Home,Search,Settings,FolderPlus} from 'lucide-angular';
+import { SettingSystemComponent } from './componets/setting-system/setting-system.component';
+import { CreateContractComponent } from './componets/create-contract/create-contract.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearcContractsComponent,
+    SidebarComponent,
+    SettingSystemComponent,
+    CreateContractComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    LucideAngularModule.pick({Home,Search,Settings,
+      FolderPlus})
   ],
   providers: [],
   bootstrap: [AppComponent]
